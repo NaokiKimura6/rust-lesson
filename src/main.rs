2a1abc4e2bba9991add984541696a81b42d1afa2
Naoki Kimura <n-kimura@fores.jp>
2022-01-15 06:50:22 +0900

    module基本操作

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// import文はmod
+mod vars;
+
 fn main() {
     println!("Hello, world!");
+    // vars::runでvars.tsのrun関数を実行
+    vars::run();
+    vars::sub_a::func_a();
+    vars::sub_b::func_b();
 }
